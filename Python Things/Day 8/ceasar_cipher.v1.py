5f577b0d8c8ab34d0758c17e1137b4208505082f
Kaylieston <nguyet.truongm@gmail.com>
2025-02-10 16:26:58 +0700

    Update while loop to continue the function

--- Python Things/Day 8/ceasar_cipher.v1.py
+++ Python Things/Day 8/ceasar_cipher.v1.py
@@ -66,6 +66,27 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 
 # Another way to solve the problem without using the While loop
 
+print("""
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88          
+              88                                             
+              88             
+      
+      """)
+
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n ").strip().lower()      
 original_text = input("Type your message:\n").lower()
 shift_amount = int(input("Type the shift number:\n"))
@@ -85,7 +106,6 @@ def encrypt(original_text, shift_amount):
     # Use return instead of print to ensure that the function is return 
 
 
-
 def cipher(original_text, shift_amount, direction):
     
     output_text = ""
@@ -99,7 +119,7 @@ def cipher(original_text, shift_amount, direction):
                 output_text += alphabet[shifted_position]
             else: 
                 output_text += letter
-        print(f"Here is the decoded result: {output_text}") 
+        return(f"Here is the decoded result: {output_text}") # Shoud use return instead of print since cipher() does not return any value 
         
     elif direction =="encode":
         print(encrypt(original_text, shift_amount))
@@ -108,16 +128,45 @@ def cipher(original_text, shift_amount, direction):
             
 cipher(original_text, shift_amount, direction)
 
-next_action = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+# Check if the user wants to go again - SOLUTION 1
+should_continue = True
 
-if next_action in ["yes", "no"]:
-    if next_action == "yes":
+while should_continue:
+    restart = input("========================\nType 'yes' if you want to go again. Otherwise type 'no'\n")
+    if restart == "no":
+        should_continue = False
+        print("Thank you for using our service. Goodbye!") 
+    elif restart == "yes":  
         encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n ").strip().lower()      
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
-        print(cipher(original_text=text, shift_amount=shift, direction=encode_or_decode))
-    elif next_action == "no":
-        print("Thank you for using our service. Goodbye!") 
+    
+        result = cipher(original_text=text, shift_amount=shift, direction=encode_or_decode) # Modify cipher() to result instead of printing inside the function, we should update the function call 
+        print(result)
+    else:
+        print("INVALID INPUT.\n ========================")
+        
 
-else:
-    print("INVALID INPUT.\n ========================")  
+# # Check if the user wants to go again - SOLUTION 22 - Could not repeat the cipher continously
+# next_action = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+
+# if next_action in ["yes", "no"]:
+#     if next_action == "yes":
+#         encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n ").strip().lower()      
+#         text = input("Type your message:\n").lower()
+#         shift = int(input("Type the shift number:\n"))
+        
+#         result = cipher(original_text=text, shift_amount=shift, direction=encode_or_decode) # Modify cipher() to result instead of printing inside the function, we should update the function call 
+#         print(result)
+#     elif next_action == "no":
+#         print("Thank you for using our service. Goodbye!") 
+
+# else:
+#     print("INVALID INPUT.\n ========================")  
+
+
+        
+
+    
+    
+    
